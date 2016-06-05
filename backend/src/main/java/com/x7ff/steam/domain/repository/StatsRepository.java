e58af7d25737a572d832d6caf9d703b41661fc63
Cube <cubeee.gh@gmail.com>
2016-06-05 15:56:00 +0300

    Remove notes and clean imports

--- backend/src/main/java/com/x7ff/steam/domain/repository/StatsRepository.java
+++ backend/src/main/java/com/x7ff/steam/domain/repository/StatsRepository.java
@@ -1,8 +1,6 @@
 package com.x7ff.steam.domain.repository;
 
-
 import java.util.List;
-import java.util.Map;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -12,9 +10,6 @@ import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.primitives.Ints;
-import com.x7ff.steam.domain.Game;
 import com.x7ff.steam.domain.GameSnapshot;
 import com.x7ff.steam.domain.MostPlayedGame;
 import com.x7ff.steam.domain.Player;
@@ -46,16 +41,6 @@ public class StatsRepository extends SimpleJpaRepository<Player, Long> {
 		return minutes == null ? 0 : minutes.longValue();
 	}
 
-	/**
-	 * SELECT
-	 *  g.name,
-	 *  SUM(s.minutes_played) sum
-	 * FROM game_snapshot AS s
-	 * LEFT JOIN game AS g ON s.game_id = g.app_id WHERE s.minutes_played > 0
-	 * GROUP BY g.name
-	 * ORDER BY sum DESC
-	 * LIMIT 10;
-	 */
 	@CacheEvict(value = "players")
 	public List<MostPlayedGame> getMostPlayedGames() {
 		List<MostPlayedGame> games = Lists.newArrayList();
