b62226886f1b5b3d96370479dab44f50d1067a77
Cube <cubeee.gh@gmail.com>
2016-06-11 19:46:55 +0300

    Add finding players by identifier

--- backend/src/main/java/com/x7ff/steam/domain/repository/PlayerRepositoryImpl.java
+++ backend/src/main/java/com/x7ff/steam/domain/repository/PlayerRepositoryImpl.java
@@ -2,6 +2,9 @@ package com.x7ff.steam.domain.repository;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.Query;
+
 import com.x7ff.steam.domain.Player;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,5 +19,17 @@ public class PlayerRepositoryImpl extends SimpleJpaRepository<Player, Long> {
 		super(Player.class, entityManager);
 		this.entityManager = entityManager;
 	}
+
+	public Player findByIdentifier(String identifier) {
+		try {
+			return (Player) entityManager
+					.createQuery("SELECT p FROM Player p WHERE p.identifier = :identifier")
+					.setParameter("identifier", identifier)
+					.getSingleResult();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
 
 }
