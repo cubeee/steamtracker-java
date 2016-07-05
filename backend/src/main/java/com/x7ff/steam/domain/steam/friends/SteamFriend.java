8aa20189204ad49982effffeb6eb70ad12b26ab4
Cube <cubeee.gh@gmail.com>
2016-07-05 21:34:43 +0300

    Implement Steam user friend list API

---
+++ backend/src/main/java/com/x7ff/steam/domain/steam/friends/SteamFriend.java
@@ -0,0 +1,50 @@
+package com.x7ff.steam.domain.steam.friends;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SteamFriend {
+
+	@JsonProperty("steamid")
+	private String steamId;
+
+	@JsonProperty("relationship")
+	private String relationship;
+
+	@JsonProperty("friend_since")
+	private long friendSince;
+
+	@JsonProperty("steamid")
+	public String getSteamId() {
+		return steamId;
+	}
+
+	@JsonProperty("steamid")
+	public void setSteamId(String steamId) {
+		this.steamId = steamId;
+	}
+
+	@JsonProperty("relationship")
+	public String getRelationship() {
+		return relationship;
+	}
+
+	@JsonProperty("relationship")
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+
+	@JsonProperty("friend_since")
+	public long getFriendSince() {
+		return friendSince;
+	}
+
+	@JsonProperty("friend_since")
+	public void setFriendSince(long friendSince) {
+		this.friendSince = friendSince;
+	}
+
+}
